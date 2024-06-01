3a6d5dee4126373447cf2cc35629fd630d05a225
cubidev <cubidev.vitu@gmail.com>
2024-06-01 13:01:57 -0300

    Changed counter to have an Min and default value, and a get/set for current value

--- addons/useful_nodes/Nodes/Counter.gd
+++ addons/useful_nodes/Nodes/Counter.gd
@@ -1,7 +1,7 @@
 @icon("res://addons/useful_nodes/Icons/Counter.svg")
 class_name Counter
 extends Node
-## An Integer Counter that goes from 0 to Max.
+## An Integer Counter that goes from Min to Max.
 
 
 ## Gets emitted when count value changes.
@@ -10,49 +10,96 @@ signal changed(difference: int)
 
 ## Gets emitted when counter reaches it maximum value.
 ## Wont get emitted unless count changes.
-signal maxed
+signal maxed_out
 
 ## Gets emitted when counter reaches zero, its minimum value.
 ## Wont get emitted unless count changes.
-signal zeroed
+signal minimized
 
 
-## Maximum Count value.
-## Changing this value can trigger Changed, Maxed and Zeroed signals.
+## Maximum Count Value.
+## Changing this value can trigger Changed, Maxed_Out and Minimized signals.
 @export_range(0, 1000000) var max: int = 10:
 	set(value): 
-		max = max(0, value)
-		__set_counter(clamp(__current, 0, max))
-		
+		max = max(min, value)
+		__set_counter(clampi(__current, min, max))
+
 	get: 
 		return max
+		
+		
+## Minimum Count Value.
+## Changing this value can trigger Changed, Maxed_Out and Minimized signals.
+@export var min: int = 0:
+	set(value):
+		min = min(max, value)
+		__set_counter(clampi(__current, min, max))
+
+	get:
+		return min
 
 
-## Private Attribute. Do Not Directly Modify It. Use get_current_count() instead.
-## Internal current count value.
+## default value of counter.
+## Use reset() to set current count back to this value.
+## default cannot go below zero nor higher than max
+@export_range(0, 1000000) var default: int = 0:
+	set(value):
+		default = clampi(default, min, max)
+		
+	get:
+		return default
+
+
+## Current count value. 
+## Use this to count up or down and get the counted value.
+## current cannot go below zero nor higher than max
+## Can trigger Changed, Maxed_Out and Minimized signals.
+var current: int = 0:
+	set(value):
+		__set_counter(clampi(value, min, max))
+
+	get:
+		return __current
+
+
 var __current: int = 0
 
 
-## Counts up or down by an amount value.
-## Can trigger Changed, Maxed and Zeroed signals.
-func count(amount: int = 1):
-	__set_counter(clampi(__current + amount, 0, max))
+func _ready():
+	default = clampi(default, min, max)
+	__current = default
+
+
+## Counts up if amount is higher than 0
+## Does nothing otherwise
+func count_up_or_nothing(amount: int = 1):
+	__set_counter(clampi(__current + amount, __current, max))
+
+
+## Counts down if amount is higher than 0
+## Does nothing otherwise
+func count_down_or_nothing(amount: int = 1):
+	__set_counter(clampi(__current + amount, min, __current))
 
 
 ## Sets current count to maximum value.
-## Can Trigger Changed and Maxed if counter isnt already maxed.
+## Can Trigger Changed and Maxed_Out if counter isnt already maxed_out.
 func fill():
 	__set_counter(max)
 
 
-## Sets current count to zero.
-## Can Trigger Changed and Zeroed if counter isnt already zeroed.
+## Sets current count to minimum value.
+## Can Trigger Changed and Minimized if counter isnt already at minimum.
 func clear():
-	__set_counter(0)
+	__set_counter(min)
+	
+
+## Sets current count back to default.
+## Can Trigger Changed, Maxed_Out or Minimized
+func reset():
+	__set_counter(default)
 
 
-## Private Function. Should Not be directly Called or Overridden.
-## Sets a new value for current count and emits appropriate signals.
 func __set_counter(new: int):
 	var difference = new - __current
 	__current = new
@@ -63,28 +110,29 @@ func __set_counter(new: int):
 	changed.emit(difference)
 	
 	if __current == max:
-		maxed.emit()
+		maxed_out.emit()
 		return
 		
-	if __current == 0:
-		zeroed.emit()
+	if __current == min:
+		minimized.emit()
 		return
 
 
-## Returns current counted value
-func get_current_count() -> int:
-	return __current
+## Returns total size of counter
+func get_size() -> int:
+	return max - min
 
 
 ## Returns completion percentage of counter.
-## If max is zero, this will return 1.0.
+## If current size is zero, this will return 1.0.
 func get_completion_percentage() -> float:
-	if max == 0: return 1.0
-	return (__current as float) / (max as float)
+	var size: int = get_size()
+	if size == 0: return 1.0
+	return (__current as float) / (size as float)
 
 
 ## Returns remaining percentage of counter.
-## if max is zero, this will return 0.0.
+## if current size is zero, this will return 0.0.
 ## Is equal to 1.0 - get_completion_percentage().
 func get_remaining_percentage() -> float:
 	return 1.0 - get_completion_percentage()
